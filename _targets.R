fe4abbaae062e711c094ef5317e3e20aad24e974
Sebastian Sauer <ssauer@posteo.de>
2024-02-07 10:41:53 +0100

    updated pipeline

--- _targets.R
+++ _targets.R
@@ -1,7 +1,7 @@
 library(targets)
 
 # packages available for all targets:
-tar_option_set(packages = c("dplyr", "purrr", "stringr", "readr"))
+tar_option_set(packages = c("dplyr", "purrr", "readr"))
 
 # source funs:
 funs_files <- list.files(path = "funs", pattern = "\\.R", full.names = TRUE)
@@ -9,18 +9,20 @@ lapply(X = funs_files, FUN = source)
 
 # targets, ie., steps to be computed:
 list(
-  tar_target(data_path_raw, yaml::read_yaml("config.yaml")$data_23ws),
+  tar_target(data_path_raw, read_yaml("config.yaml")$data_23ws, 
+             packages = "yaml"),
   tar_target(data_files_list, list.files(path = data_path_raw,
                                          full.names = TRUE,
                                          recursive = TRUE), format = "file"),
   tar_target(data_files_dupes_excluded, exclude_dupes(data_files_list)),
+  tar_target(data_files_no_json, exclude_filetype(data_files_dupes_excluded, "json")),
   tar_target(data_imported, 
-             data_files_dupes_excluded |> 
+             data_files_no_json |> 
                map(import_data) |> 
-               list_rbind()),
+               list_rbind(), packages = c("lubridate", "stringr")),
   tar_target(data_wo_empty_cols, 
-             deselect_empty_cols(data_imported)),
-  tar_target(data_w_num_cols,
-             data_wo_empty_cols |> mutate(across(everything(), parse_number)))
+             remove_empty(data_imported, which = c("rows", "cols")), packages = "janitor")
+  # tar_target(data_w_num_cols,
+  #            data_wo_empty_cols |> mutate(across(everything(), parse_number)))
   
 )
