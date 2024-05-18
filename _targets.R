c38f780c1eeb69b4d7507af12290458228885b53
Dr. Sebastian Sauer <sebastian.sauer@hs-ansbach.de>
2024-05-18 21:56:36 +0200

    include 24-ss data

--- _targets.R
+++ _targets.R
@@ -1,9 +1,17 @@
+# MASTER PIPELINE HANS LOG DATA ANALYSIS
+# author: Sebastian Sauer
+
+
+# setup -------------------------------------------------------------------
 library(targets)
 library(dplyr)
 library(tarchetypes)
+#library(crew)
 
 # packages available for all targets:
-tar_option_set(packages = c("dplyr", "purrr", "readr", "tidyr"))
+tar_option_set(
+  packages = c("dplyr", "purrr", "readr", "tidyr")
+  )
 
 
 # source funs:
@@ -12,18 +20,19 @@ funs_files <- list.files(
 lapply(X = funs_files, FUN = source)
 
 
+# import data -------------------------------------------------------------
+
 # targets, ie., steps to be computed:
 list(
   # read data path as saved in config.yaml:
   tar_target(config_file, "config.yaml", 
              format = "file"),  # watch config file
-  tar_target(paths, read_yaml(config_file), 
-             packages = "yaml"),
-  tar_target(data_path_raw, paths$data, 
+  tar_target(config, read_yaml(config_file), 
              packages = "yaml"),
   tar_target(data_files_list, 
-             list.files(path = data_path_raw,
+             list.files(path = config$data,
                         full.names = TRUE,
+                        pattern = config$data_raw_pattern,
                         recursive = TRUE), 
              format = "file"),  # watch data source files
   
@@ -32,15 +41,20 @@ list(
              exclude_dupes(data_files_list)),
   
   # exclude json files:
-  tar_target(data_files_no_json, 
+  tar_target(data_files_no_json,
              exclude_filetype(data_files_dupes_excluded, "json")),
   
   # import data files and bind in one df:
   tar_target(data_imported, 
              data_files_no_json |> 
                map(import_data) |> 
-               list_rbind(), 
-             packages = c("lubridate", "stringr", "data.table")),
+               rbindlist(fill = TRUE), 
+             packages = c("lubridate", "stringr", "dplyr", "data.table")),
+  
+  
+  
+
+# prep data ---------------------------------------------------------------
   
   # remove empty cols:
   tar_target(data_wo_empty_cols, 
@@ -65,11 +79,13 @@ list(
   
   # transform all columns to character:
   tar_target(data_all_chr, 
-             data_no_img_cols |> mutate(across(everything(), as.character))),
+             data_no_img_cols |> mutate(across(everything(), as.character)),
+             packages = "collapse"),
   
   # transform all cols to factor: 
   tar_target(data_all_fct,
-             data_all_chr |> mutate(across(everything(), factor))),
+             data_all_chr |> mutate(across(everything(), as.factor)),
+             packages = "collapse"),
   
   # exclude non-participants:
   tar_target(data_users_only,
@@ -77,7 +93,24 @@ list(
                filter(!str_detect(actiondetails_0_url, 
                                   "=admin|=developer|=lecturer")) |> 
                filter(!str_detect(actiondetails_1_subtitle, 
-                                  "=admin|=developer|=lecturer"))),
+                                  "=admin|=developer|=lecturer")),
+             packages = "stringr"),
+
+
+
+
+# tinify data -------------------------------------------------------------
+
+  tar_target(data_less_cols,
+             data_users_only |> 
+               select(-c(contains("idpageview"), 
+                         contains("pretty"),
+                         contains("pageviewposition"),
+                         contains("pageid"),
+                         #select(-matches("\\w+_(?!0)\\d+_timestamp")),
+                         contains("timespent")))),
+
+
   
   # prepare little data set for quick checking:
   tar_target(data_little,
@@ -95,11 +128,30 @@ list(
   tar_target(data_user1_long,  # transform into long format
              longify_data(data_user1),
              packages = "collapse"),
+
+tar_target(data_wide_slim,
+           data_less_cols |> 
+           get_vars(vars = c("idvisit", 
+                             grep("actiondetails_", names(data_less_cols),
+                                  value = TRUE))),
+           packages = "collapse"),
+
+
+
+# pivot longer ------------------------------------------------------------
+
+  tar_target(data_long,
+             data_wide_slim |> 
+               pivot(ids = "idvisit",
+                     how = "longer",
+                     check.dups = TRUE, 
+                     factor = FALSE),
+             packages = "collapse"),
   
   # pivot longer to get a handle on the number of cols per login:
-  tar_target(data_long,
-             data_all_chr |> longify_data(),
-             packages = "collapse"),
+  # tar_target(data_long2,
+  #            data_all_chr |> longify_data(),
+  #            packages = "collapse"),
   
   tar_target(data_long_nona,
              data_long |> drop_na()),  # drop rows with missing data
@@ -108,11 +160,25 @@ list(
   tar_target(data_slim,
              slimify_nona_data(data_long_nona), 
              packages = c("dplyr", "tidyr", "collapse")),
+
+
+
+# Mini slim data for debugging --------------------------------------------
+
+
+
+
   tar_target(data_user1_long_slim,
               slimify_nona_data(data_user1_long), 
              packages = c("dplyr", "tidyr", "collapse")),
   tar_target(data_slim_head,
              data_slim[1:1e5,]),
+
+
+
+
+
+# count stuff per visit -------------------------------------------------
 
   # count rows per visit (n):
   tar_target(count_action,
@@ -133,6 +199,7 @@ list(
                select(idvisit, visitduration) %>% 
                mutate(visitduration_sec = as.numeric(visitduration)) %>% 
                select(-visitduration)),
+
   # count time of visit per weekday:
   tar_target(time_visit_wday,
              data_slim |> when_visited(), 
@@ -142,32 +209,37 @@ list(
                select(idvisit, dayssincelastvisit)
              ),
   
-  
   # count action categories per visit:
   tar_target(count_action_type,
              count_user_action_type(data_slim), packages = "stringr"),
   
 
+
+
+
+# render report in Quarto -------------------------------------------------
+
+
   
   # render report:
-  tar_quarto(report01, "report01.qmd"),
+  tar_quarto(report01, "report01.qmd")
   
   # export processed data to disk as RDS file:
-  tar_target(data_to_be_exported, 
-             list(
-               time_spent = time_spent, 
-               data_slim = data_slim, 
-               count_action = count_action,
-               data_little = data_little,
-               data_all_chr = data_all_chr,
-               data_all_fct = data_all_fct,
-               data_user1 = data_user1,
-               data_user1_long = data_user1_long,
-               data_little_long = data_little_long,
-               data_slim_head = data_slim_head)),
-  tar_target(export_data, 
-             save_data_as_rds(
-               data_to_be_exported, config_file),
-             packages = c("purrr", "yaml"))
+  # tar_target(data_to_be_exported, 
+  #            list(
+  #              time_spent = time_spent, 
+  #              data_slim = data_slim, 
+  #              count_action = count_action,
+  #              data_little = data_little,
+  #              data_all_chr = data_all_chr,
+  #              data_all_fct = data_all_fct,
+  #              data_user1 = data_user1,
+  #              data_user1_long = data_user1_long,
+  #              data_little_long = data_little_long,
+  #              data_slim_head = data_slim_head)),
+  # tar_target(export_data, 
+  #            save_data_as_rds(
+  #              data_to_be_exported, config_file),
+  #            packages = c("purrr", "yaml"))
             
 )
