9556a1360dc674fc03daec3c6908160f6597d288
Sebastian Sauer <ssauer@posteo.de>
2025-06-23 23:45:25 +0200

    fix non-unique ids

--- funs/slimify_data.R
+++ funs/slimify_data.R
@@ -3,15 +3,15 @@ slimify_nona_data <- function(data_long) {
   out <- 
     data_long |> 
     # prepare to count the number of things a user does:
-    fselect(variable, value, idvisit) |> 
+    select(variable, value, idvisit) |> 
     separate(variable, sep = "_", into = c("constant", "nr", "type")) |> 
-    fselect(-c(constant)) |> 
-    ftransform(nr = as.integer(nr),
+    select(-c(constant)) |> 
+    mutate(nr = as.integer(nr),
                idvisit = as.integer(idvisit),
                type = factor(type),
                value = as.character(value)) |> 
     #ungroup() |> 
-    roworder(idvisit, nr)
+    arrange(idvisit, nr)
     
   return(out)
 
--- funs/time-min-max.R
+++ funs/time-min-max.R
@@ -5,7 +5,8 @@ time_min_max <- function(data) {
     mutate(type = as.character(type)) |> 
     mutate(time = parse_date_time(value, "ymd HMS")) |> 
     summarise(time_min = min(time),
-              time_max = max(time))
+              time_max = max(time),
+              time_diff = time_max - time_min)
 }
 
 
