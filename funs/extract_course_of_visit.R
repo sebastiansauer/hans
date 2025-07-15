29dea3f89207e14f356cccf2f6da3ad20ec2a4f1
Sebastian Sauer <ssauer@posteo.de>
2025-07-15 16:13:32 +0200

    update

--- funs/extract_course_of_visit.R
+++ funs/extract_course_of_visit.R
@@ -1,5 +1,9 @@
-extract_course_of_visit <- function(d = data_wide_slim){
-d |> # data such as "data_wide_slim"
-  select(actiondetails_0_url, actiondetails_0_timestamp) |> 
-  mutate(course = str_extract(actiondetails_0_url, "(?<=\\.student\\.)[a-zA-Z0-9]+"))
+extract_course_role_university_of_visit <- function(d = data_wide_slim){
+d |> # WIDE data such as "data_wide_slim"
+  select(idvisit,actiondetails_0_url, actiondetails_0_timestamp) |> 
+  mutate(course = str_extract(actiondetails_0_url, "(?<=\\.student\\.)[a-zA-Z0-9]+")) |> 
+  mutate(university = str_extract(actiondetails_0_url, "(?<=%40)[a-z0-9-]+")) |> 
+  mutate(role = str_extract(actiondetails_0_url, "(?<=role=)[a-z]+")) |> 
+  mutate(actiondetails_0_timestamp = ymd_hms(actiondetails_0_timestamp)) |> 
+  select(-actiondetails_0_url)
 }
