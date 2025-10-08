0d396da0f8062524a076c503e43a63536a0b5568
Sebastian Sauer <ssauer@posteo.de>
2025-10-08 16:27:54 +0200

    rebuild

--- R/get_llm_response_text.R
+++ R/get_llm_response_text.R
@@ -1,7 +1,7 @@
 get_llm_response_text <- function(d = n_action_type) {
   d |>
     filter(str_detect(value, "llm_response")) |>
-    select(value) |>
+    select(idvisit, value) |>
     mutate(
       lang = str_extract(value, "llm_response_([\\w]+)", group = 1),
       tokens_n = lengths(tokenize_words(value))
